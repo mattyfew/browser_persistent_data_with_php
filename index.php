e1928fd3ac402fd3809456306a2e7cb81260ba87
Matt Fewer <mattfewer@hotmail.com>
2018-12-03 20:46:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include 'inc/header.php';
+
+echo '<h1>Welcome to our<br />Treehouse Story Game</h1>';
+echo '<p>Enter the requested words and create your story.</p>';
+echo '<p><a class="btn btn-default btn-lg" href="play.php" role="button">Play</a></p>';
+
+echo '<h2>Reread Your Saved Stories</h2>';
+
+include 'inc/footer.php';
---
+++ story.php
@@ -0,0 +1,24 @@
+<?php
+$word1 = '__';
+$word2 = '__';
+$word3 = '__';
+$word4 = '__';
+$word5 = '__';
+
+include 'inc/header.php';
+
+echo '<h1>My Treehouse Story</h1>';
+
+echo '<p>There once was a(n) ' . $word1;
+echo ' programmer named ' . $word2; 
+echo '. </p>';
+echo '<p>This ' .  $word3; 
+echo ' programmer used Treehouse to learn to ' . $word4;
+echo ' the ' . $word5 . '.</p>';
+
+echo ' <a class="btn btn-default btn-lg" href="#" role="button">Save Story</a>';
+echo ' <a class="btn btn-default btn-lg" href="play.php" role="button">Play Again</a>';
+echo ' <a class="btn btn-default btn-lg" href="index.php" role="button">Other Stories</a>';
+
+
+include 'inc/footer.php';
